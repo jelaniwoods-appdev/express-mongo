4897f50a682b27b16efb4be694dbcc917e5b2f24
Web Dev Simplified <webdevsimplified@gmail.com>
2019-03-11 17:12:45 -0500

    Initial Setup

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').load()
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser') // included in express 4.16+
 
 const indexRouter = require('./routes/index')
-const authorsRouter = require('./routes/authors')
-const booksRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-
-db.on('error', error => console.log(error))
-db.once('open', () => console.log('this world has been connected~'))
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
-app.use('/authors', authorsRouter)
-app.use('/books', booksRouter)
 
 app.listen(process.env.PORT || 3000)
