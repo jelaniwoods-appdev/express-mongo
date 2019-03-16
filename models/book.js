e9021e7b55320490076fc0a965031aaa82deb2a9
Web Dev Simplified <webdevsimplified@gmail.com>
2019-03-16 12:39:46 -0500

    Books Create/Index/New

--- models/book.js
+++ models/book.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const path = require('path')
 
 const coverImageBasePath = 'uploads/bookCovers'
 
@@ -10,13 +11,18 @@ const bookSchema = new mongoose.Schema({
   description: {
     type: String
   },
-  publishedDate: {
+  publishDate: {
     type: Date,
     required: true
   },
   pageCount: {
     type: Number,
     required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
   },
   coverImageName: {
     type: String,
@@ -25,16 +31,15 @@ const bookSchema = new mongoose.Schema({
   author: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref:  'Author' // must match the name
-  },
-  createdAt: {
-    type: Number,
-    type: Date,
-    required: true,
-    default: Date.now
+    ref: 'Author'
   }
 })
 
-// name the model (table name), and schema to send to db
+bookSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImageName != null) {
+    return path.join('/', coverImageBasePath, this.coverImageName)
+  }
+})
+
 module.exports = mongoose.model('Book', bookSchema)
 module.exports.coverImageBasePath = coverImageBasePath
