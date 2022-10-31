1e43caec04e162cff6cab9e1975114ae43924f6f
Jelani Woods <jelani@firstdraft.com>
2022-10-31 18:34:18 -0500

    Setup author form

--- routes/authors.js
+++ routes/authors.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 router.get('/', (req, res) => {
   res.render('authors/index')
 })
 
 router.get('/new', (req, res) => {
-  res.render('authors/new')
+  res.render('authors/new', { author: new Author() })
 })
 
 router.post('/create', (req, res) => {
