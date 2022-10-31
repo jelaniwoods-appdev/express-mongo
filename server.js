fad1dc4f285b8645c31df1ab15262dc0bdabb020
Jelani Woods <jelani@firstdraft.com>
2022-10-30 22:01:53 -0500

    Add dotenv and connect to db

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -10,6 +14,13 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('this world has been connected~'))
 
 app.use('/', indexRouter)
 
